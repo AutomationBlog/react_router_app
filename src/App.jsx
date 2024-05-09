ec881625ee6be80722361052c968a562d157cfbc
automationblog <sawin24@gmail.com>
2024-05-09 22:05:05 +0530

    route created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import Menu from "./components/menu/menu.jsx";
-import Content from "./components/content/content.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import All from "./components/content/contentAll.jsx";
+import FullStackDevelopment from "./components/content/contentFSD.jsx";
+import Career from "./components/content/career.jsx";
+import DataScience from "./components/content/contentDataScience.jsx";
+import CyberSecurity from "./components/content/contentCyberSecurity.jsx";
+
+import course from "./components/courseContent.json";
 
 function App() {
   return (
     <>
-      <Menu />
-      <Content />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<All />} />
+          <Route
+            path="/Full-Stack-Development"
+            element={<FullStackDevelopment />}
+          />
+          <Route path="/Data-Science" element={<DataScience />} />
+          <Route path="/Cyber-Security" element={<CyberSecurity />} />
+          <Route path="/Career" element={<Career />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,51 +1,33 @@
+import { Link } from "react-router-dom";
+
 export default function Menu() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid col-xl-8 mt-5">
-        <a className="navbar-brand" href="#">
-          COURSES
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">
-                ALL
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                FULL STACK DEVELOPMENT
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                DATA SCIENCE
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                CYPER SECURITY
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                CAREER
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <ul className="nav justify-content-center">
+      <li className="nav-item">
+        <Link className="nav-link" aria-current="page" href="#" to={"/"}>
+          ALL
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="#" to={"/Full-Stack-Development"}>
+          FULL STACK DEVELOPMENT
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="#" to={"/Data-Science"}>
+          DATA SCIENCE
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="#" to={"/Cyber-Security"}>
+          CYBER SECURITY
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="#" to={"/Career"}>
+          CAREER{" "}
+        </Link>
+      </li>
+    </ul>
   );
 }
